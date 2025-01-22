20c15a8e2594367f29cfd932d5eeae1b7f8bf8a0
CuriousGeorg1 <151785763+CuriousGeorg1@users.noreply.github.com>
2025-01-22 12:46:30 +0200

    Update Analysis.py
    Minor edits in language

--- Analysis.py
+++ Analysis.py
@@ -2,7 +2,7 @@
 
 from scipy.stats import norm
 
-# Arvot
+# Values
 mu = 25000  # mean
 sigma = 3000  # standard deviation
 X_a = 35000 # X-values
@@ -10,30 +10,24 @@ X_b = 20000
 z_c = 0.99 # z-value
 
 # Propability that X is bigger than limit
-def todennakoisuusSuurempaa(arvo):
-    z_a = (arvo - mu) / sigma
-    p_a = 1 - norm.cdf(z_a)  # Todennäköisyys, että z-arvo on suurempi kuin z_a
+def probabilityBigger(value):
+    z_a = (value - mu) / sigma
+    p_a = 1 - norm.cdf(z_a)
     return p_a
 
 
 
 # Propability that X is smaller than limit
-def todennakoisuusPienempaa(arvo):
+def proablilitySmaller(value):
     z_b = (arvo - mu) / sigma
-    p_b = norm.cdf(z_b)  # Todennäköisyys, että z-arvo on pienempi kuin z_b
+    p_b = norm.cdf(z_b)  
     return p_b
 
 
 
-# percentage that exceeds given limit
-def overLimit(raja):
-    z_c = norm.ppf(0.99)
-    X_c = z_c * sigma + mu  # Selvitetään X arvo
-    return X_c
-
 print(todennakoisuusSuurempaa(X_a))
 print(todennakoisuusPienempaa(X_b))
-print(overLimit(z_c))
+
 
 
 
